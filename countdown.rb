2ece4f9d137bb7650b9e2a4fde41cbdfe3e7bbc2
michael white <michaeljwhite@gmail.com>
2019-02-01 01:14:11 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,12 @@
-#write your code here
 def countdown(seconds)
-
+	
 	while seconds > 0 do
 		puts "#{seconds} SECOND(S)!"
 		seconds -= 1
 	end
 	"HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep
+	sleep 5
 end
